c2c588f10f3c007a16eaca86eef16d45eed861c7
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2025-10-28 18:27:44 -0300

    feat(): cria a janela principal

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import messagebox
+#import cvs
+
+janela = tk.Tk()
+janela.title("SISTEMA DE CADASTRAMENTO DE LIVROS")
+janela.geometry("1000x900")
+
+janela.mainloop()
